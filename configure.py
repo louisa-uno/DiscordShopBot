aa746c0235bf1c40c9ad7420d4f3cae6bb12fd74
$ ducky#6170 <122984511+ducky-g9av@users.noreply.github.com>
2024-01-05 19:41:59 +0100

    Revert "Update configure.py"

--- configure.py
+++ configure.py
@@ -6,67 +6,66 @@ from shutil import copyfile
 import discord
 import mysql.connector
 
-intents = discord.Intents.default()
-intents.all()
-
-client = discord.Client(intents=intents)
+client = discord.Client()
 
 if not os.path.isfile("config.json"):
-    copyfile("default-config.json", "config.json")
+	copyfile("default-config.json", "config.json")
 
 with open("config.json") as f:
-    config = json.load(f)
-    config_mysql = config["mysql"]
-    config_discord = config["discord"]
+	config = json.load(f)
+	config_mysql = config["mysql"]
+	config_discord = config["discord"]
 
 
 @client.event
 async def on_ready():
-    print("discord: Connection is working")
+	print("discord: Connection is working")
 
 
 while True:
-    print("\n[1] Quick installation")
-    print("[2] Test the configuration (Save the configuration before using this option.)")
-    print("[3] save/exit")
+	print("\n[1] Quick installation")
+	print(
+	    "[2] Test the configuration (Save the configuration before using this option.)"
+	)
+	print("[3] save/exit")
 
-    option = input("Please enter the desired option:")
+	option = input("Please enter the desired option:")
 
-    if option == "1":
-        config_mysql["host"] = input("Please enter your mysql host:")
-        config_mysql["port"] = input("Pleasen enter your mysql port:")
-        config_mysql["user"] = input("Please enter your mysql username:")
-        config_mysql["password"] = input("Please enter your mysql password:")
-        config_mysql["database"] = input(
-            "Please enter your mysql database name:")
+	if option == "1":
+		config_mysql["host"] = input("Please enter your mysql host:")
+		config_mysql["port"] = input("Pleasen enter your mysql port:")
+		config_mysql["user"] = input("Please enter your mysql username:")
+		config_mysql["password"] = input("Please enter your mysql password:")
+		config_mysql["database"] = input(
+		    "Please enter your mysql database name:")
 
-        config_discord["bot_token"] = input(
-            "Please enter your discord bot token:")
-    elif option == "2":
-        try:
-            test_mysql = mysql.connector.connect(
-                user=config_mysql["user"],
-                password=config_mysql["password"],
-                host=config_mysql["host"],
-                port=config_mysql["port"],
-                database=config_mysql["database"])
-            test_mysql.close()
-            print("mysql: Connection is working")
-        except mysql.connector.Error as err:
-            print(f"mysql: Something went wrong: {err}")
-        client.run(config_discord["bot_token"])
-    elif option == "3":
-        print("Do you want to save before exiting? [y/n]")
-        option = input()
-        if option == "y" or option == "yes":
-            break
-        if option == "n" or option == "no":
-            sys.exit()
-        else:
-            print("You entered an invalid option.")
+		config_discord["bot_token"] = input(
+		    "Please enter your discord bot token:")
+	elif option == "2":
+		try:
+			test_mysql = mysql.connector.connect(
+			    user=config_mysql["user"],
+			    password=config_mysql["password"],
+			    host=config_mysql["host"],
+			    port=config_mysql["port"],
+			    database=config_mysql["database"])
+			test_mysql.close()
+			print("mysql: Connection is working")
+		except mysql.connector.Error as err:
+			print(f"mysql: Something went wrong: {err}")
+		client.run(config_discord["bot_token"])
+	elif option == "3":
+		print(("Do you want to save before exiting? [y/n]"))
+		option = input()
+		if option == "y" or "yes":
+			break
+		if option == "n" or "no":
+			sys.exit()
+		else:
+			print("You entered a invalid option.")
 
-    else:
-        print("Please enter a valid option number!")
+	else:
+		print("Please enter a valid option number!")
 
 with open("config.json", "w") as f:
-    json.dump(config, f, indent=4, sort_keys=False)
+	json.dump(config, f, indent=4, sort_keys=False)
